c6212560b63870021bc78a6ed77167fd5e774d26
Shufan Li <jacklishufan@berkeley.edu>
2023-06-23 06:33:26 +0000

    fix multinode

--- cluster/build/gpu_16GBX8/build_exps_512_swin_t_bs_32.sh
+++ cluster/build/gpu_16GBX8/build_exps_512_swin_t_bs_32.sh
@@ -9,28 +9,28 @@ pbsdsh -v -- bash -l -c "module load singularity && CUDA_VISIBLE_DEVICES=0,1 sin
     --rdzv_backend c10d \
     --rdzv_endpoint "$MASTER_ADDR:$MASTER_PORT" \
     train_val_segmentor.py  \
-    --world-size 8   \
-    --distributed  \
-    --config configs/${CONFIG}.json  \
-    --workers 8  \
-    --data-dir=$DATA_DIR  \
-    --test_every 1 \
-    --shoreline-dir $SHORE_DIR \
-    --val-dir $VAL_OUT_DIR  \
-    --output-dir $VAL_OUT_DIR \
-    --folds-csv meta/folds.csv \
-    --prefix val_only_  \
-    --fold 77    \
-    --freeze-epochs 0 \
-    --fp16 --name exps_512_swin_t_bs_32 \
-    --crop_size 512 \
-    --crop_size_val 512 \
-    --overlap_val 10 \
-    --positive_ratio 0.8 \
-    --test_every 20 \
-    --epoch 240 \
-    --bs 4 \
-    --lr 0.003 \
-    --wd 1.0e-4 \
-    --drop_path 0.2 \
-    --pretrained default ;"
+        --world-size 8   \
+        --distributed  \
+        --config configs/swin.json  \
+        --workers 8  \
+        --data-dir=$XVIEW_ROOT \
+        --test_every 1 \
+        --shoreline-dir $XVIEW_ROOT/shoreline/validation \
+        --val-dir $HOME/output-exps_512_swin_t_bs_32-$ID  \
+        --output-dir $HOME/output-exps_512_swin_t_bs_32-$ID \
+        --folds-csv meta/folds.csv \
+        --prefix val_only_  \
+        --fold 77    \
+        --freeze-epochs 0 \
+        --fp16 --name exps_512_swin_t_bs_32 \
+        --crop_size 512 \
+        --crop_size_val 512 \
+        --overlap_val 10 \
+        --positive_ratio 0.8 \
+        --test_every 20 \
+        --epoch 240 \
+        --bs 4 \
+        --lr 0.003 \
+        --wd 1.0e-4 \
+        --drop_path 0.2 \
+        --pretrained default ;
--- cluster/run_cluster_multinode.sh
+++ cluster/run_cluster_multinode.sh
@@ -9,28 +9,28 @@ pbsdsh -v -- bash -l -c "module load singularity && CUDA_VISIBLE_DEVICES=$DEVICE
     --rdzv_backend c10d \
     --rdzv_endpoint "$MASTER_ADDR:$MASTER_PORT" \
     train_val_segmentor.py  \
-    --world-size $NUM_GPUS   \
-    --distributed  \
-    --config configs/${CONFIG}.json  \
-    --workers 8  \
-    --data-dir=$DATA_DIR  \
-    --test_every 1 \
-    --shoreline-dir $SHORE_DIR \
-    --val-dir $VAL_OUT_DIR  \
-    --output-dir $VAL_OUT_DIR \
-    --folds-csv meta/folds.csv \
-    --prefix val_only_  \
-    --fold $FOLD    \
-    --freeze-epochs 0 \
-    --fp16 --name $NAME \
-    --crop_size $CROP \
-    --crop_size_val $CROP_VAL \
-    --overlap_val 10 \
-    --positive_ratio $SAMPLE_RATE \
-    --test_every 20 \
-    --epoch $EPOCH \
-    --bs $BS \
-    --lr $LR \
-    --wd $WD \
-    --drop_path $DROP_PATH \
-    --pretrained $PRETRAINED ;"
+        --world-size $NUM_GPUS   \
+        --distributed  \
+        --config configs/$CONFIG.json  \
+        --workers 8  \
+        --data-dir=$XVIEW_ROOT \
+        --test_every 1 \
+        --shoreline-dir $XVIEW_ROOT/shoreline/validation \
+        --val-dir $HOME/output-$NAME-$ID  \
+        --output-dir $HOME/output-$NAME-$ID \
+        --folds-csv meta/folds.csv \
+        --prefix val_only_  \
+        --fold $FOLD    \
+        --freeze-epochs 0 \
+        --fp16 --name $NAME \
+        --crop_size $CROP \
+        --crop_size_val $CROP_VAL \
+        --overlap_val 10 \
+        --positive_ratio $SAMPLE_RATE \
+        --test_every 20 \
+        --epoch $EPOCH \
+        --bs $BS \
+        --lr $LR \
+        --wd $WD \
+        --drop_path $DROP_PATH \
+        --pretrained $PRETRAINED ;
