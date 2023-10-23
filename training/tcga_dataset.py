373dd684214f8929e2a5e3aba48c8ab65d3af769
Ritwik Gupta <ritwikg2004@live.com>
2023-10-23 05:46:31 +0000

    Add TCGA dataloader

--- training/tcga_dataset.py
+++ training/tcga_dataset.py
@@ -12,13 +12,13 @@ import torch
 from rasterio.windows import Window
 from torch.utils.data import Dataset
 
+import openslide
 
-def slide_name_to_patient_barcode(slide_name):
+def slide_fname_to_patient_barcode(slide_name):
     """
     Take a slide filepath and return the corresponding patient barcode
     """
     return "-".join(slide_name.stem.split("-")[:3])
-
 
 class TCGADataset(Dataset):
     def __init__(
@@ -35,7 +35,7 @@ class TCGADataset(Dataset):
             mode: Can be either "train" or "val"
         """
         self.dataset_dir = dataset_dir
-        self.folds = pd.read_csv(folds_csv)
+        self.folds = pd.read_csv(folds_csv) if folds_csv else None
         self.labels = pd.read_csv(annotation_csv)
         self.files = pd.Series(Path(self.dataset_dir).glob("tcga_*/WSIs/*.svs"))
 
@@ -55,23 +55,49 @@ class TCGADataset(Dataset):
         """
         Only keep the files specified by the folds and make them into a lookup table
         """
-        # Filter on splits
-        # Drop the first index column of both folds and labels
-        labels = pd.merge(folds.iloc[:, 1:], labels.iloc[:, 1:], how="inner", left_on=self.mode, right_on="bcr_patient_barcode")
+        if self.folds is not None:
+            # Filter on splits
+            # Drop the first index column of both folds and labels
+            labels = pd.merge(folds.iloc[:, 1:], labels.iloc[:, 1:], how="inner", left_on=self.mode, right_on="bcr_patient_barcode")
 
-        if self.mode == "train":
-            labels = labels.drop("val", axis=1)
-        elif self.mode == "val":
-            labels = labels.drop("train", axis=1)
-        
-        # This column is redundant
-        labels = labels.drop("bcr_patient_barcode", axis=1)
+            if self.mode == "train":
+                labels = labels.drop("val", axis=1)
+            elif self.mode == "val":
+                labels = labels.drop("train", axis=1)
+            
+            # This column is redundant
+            labels = labels.drop("bcr_patient_barcode", axis=1)
 
-        # Convert into a lookup table
-        labels = labels.rename(columns={self.mode: "barcode"})
-        labels = labels.set_index("barcode").to_dict("index")
-        return labels
-
+            # Convert into a lookup table
+            labels = labels.rename(columns={self.mode: "barcode"})
+            labels = labels.set_index("barcode").to_dict("index")
+            return labels
+        else:
+            labels = labels.iloc[:, 1:]
+            labels = labels.rename(columns={"bcr_patient_barcode": "barcode"})
+            labels = labels.set_index("barcode").to_dict("index")
+            return labels
 
     def __getitem__(self, i):
-        pass
+        fname = self.files.iloc[i]
+        barcode = slide_fname_to_patient_barcode(fname)
+        label = self.labels[barcode]
+
+        slide = openslide.OpenSlide(fname)
+        try:
+            x_res_mpp = slide.properties['openslide.mpp-x']
+            y_res_mpp = slide.properties['openslide.mpp-y']
+        except KeyError:
+            x_res_mpp = None
+            y_res_mpp = None
+
+        image = slide.read_region(location=(0, 0), level=0, size=slide.dimensions)
+        #image = image.convert('RGB')
+        image = np.array(image)
+
+        return {
+            "image": image,
+            **label,
+            "x_res_mpp": x_res_mpp,
+            "y_res_mpp": y_res_mpp
+        }
