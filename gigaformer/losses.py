6ddff8849fcea4da801955e79cf9af0306c78fbf
Tyler Zhu <tyler.zhu@berkeley.edu>
2023-11-08 22:15:45 +0000

    minor typo.

--- gigaformer/losses.py
+++ gigaformer/losses.py
@@ -61,7 +61,7 @@ def build_losses(config: LossConfig) -> List[LossFunction]:
             loss_func = CenterLossCalculator()
         elif loss_type == "length":
             loss_func = LengthLoss()
-        elif loss_type == 'crossentrophy':
+        elif loss_type == 'crossentropy':
             loss_func = CrossEntrophy(**single_loss.params)
         else:
             raise ValueError(f"Unknown loss type {loss_type}")
