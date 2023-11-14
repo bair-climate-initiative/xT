7b8d8280c128c5d634b319820ecab640a0ce7859
Ritwik Gupta <ritwikg2004@live.com>
2023-11-14 09:05:21 +0000

    Fix swin v2 configs and set batch size

--- gigaformer/datasets/inaturalist.py
+++ gigaformer/datasets/inaturalist.py
@@ -44,9 +44,7 @@ class INatDataset(Dataset):
         self.channels_first = channels_first
         self.mode = mode
         if self.mode not in ["train", "val"]:
-            raise NotImplementedError(
-                "Please provide a valid mode ('train', 'val')."
-            )
+            raise NotImplementedError("Please provide a valid mode ('train', 'val').")
 
         self.transforms = transforms
 
@@ -56,21 +54,27 @@ class INatDataset(Dataset):
     def __getitem__(self, idx):
         label = self.labels[idx]
         img_path = self.dataset_dir / label["file_name"]
-        img = Image.open(img_path).convert('RGB')
+        img = Image.open(img_path).convert("RGB")
         if self.transforms:
             img = self.transforms(img)
         img = np.asarray(img)
         if not self.channels_first:
-            img = img.transpose((1, 2, 0))
+            img = img.transpose((2, 0, 1))
 
         return {"image": img, **label}
 
-    def _process_labels(self, labels: COCO, supercategories: Set[str], ):
+    def _process_labels(
+        self,
+        labels: COCO,
+        supercategories: Set[str],
+    ):
         """
         Load keys, images, and annotations
         """
         ids = sorted(list(labels.anns.keys()))
-        valid_category_ids = set([x["id"] for x in self.categories if x["supercategory"] in supercategories])
+        valid_category_ids = set(
+            [x["id"] for x in self.categories if x["supercategory"] in supercategories]
+        )
         if self.category_label_map is None:
             category_label_map = {}
         else:
@@ -87,16 +91,18 @@ class INatDataset(Dataset):
                 category_label_map[label] = counter
                 counter += 1
 
-            ret_labels.append({
-                "id": id,
-                "file_name": labels.imgs[id]["file_name"],
-                "label": category_label_map[label],
-                "id": labels.imgs[id]["id"],
-            })
-        
+            ret_labels.append(
+                {
+                    "id": id,
+                    "file_name": labels.imgs[id]["file_name"],
+                    "label": category_label_map[label],
+                    "id": labels.imgs[id]["id"],
+                }
+            )
+
         self.category_label_map = category_label_map
 
         return ret_labels
-    
+
     def output_to_category(self, model_output):
         return self.label_category_map[model_output]
