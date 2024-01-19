3f2cf077647654e2d7ad9d7b9dee79551822b475
Ritwik Gupta <ritwikg2004@live.com>
2024-01-19 03:32:22 +0000

    Add swin small and tiny

--- gigaformer/models/backbones/swin_timm.py
+++ gigaformer/models/backbones/swin_timm.py
@@ -1,25 +1,46 @@
 import timm
 from torch import nn
-class SwinWrapper(nn.Module):
 
-    def __init__(self,model,hidden_size=768):
+
+class SwinWrapper(nn.Module):
+    def __init__(self, model, hidden_size=768):
         super().__init__()
         self.model = model
         # self.model.head = nn.Identity()
         # self.model.norm = nn.Identity()
         self.feature_info = list(model.feature_info)
-    def forward(self,x):
-        intermediates = self.model(x)
-        intermediates = list([x.permute(0,3,1,2) for x in intermediates])
-        return intermediates
-    
-    
 
-def swinv2_base_window16_256_timm(*args,**kwargs):
-    model = timm.create_model('swinv2_base_window16_256.ms_in1k',features_only=True,pretrained=True)
+    def forward(self, x):
+        intermediates = self.model(x)
+        intermediates = list([x.permute(0, 3, 1, 2) for x in intermediates])
+        return intermediates
+
+
+def swinv2_tiny_window16_256_timm(*args, **kwargs):
+    model = timm.create_model(
+        "swinv2_tiny_window16_256.ms_in1k", features_only=True, pretrained=True
+    )
     return SwinWrapper(model)
 
 
-def swinv2_large_window16_256_timm(*args,**kwargs):
-    model = timm.create_model('swinv2_large_window12to16_192to256.ms_in22k_ft_in1k',features_only=True,pretrained=True)
+def swinv2_small_window16_256_timm(*args, **kwargs):
+    model = timm.create_model(
+        "swinv2_small_window16_256.ms_in1k", features_only=True, pretrained=True
+    )
+    return SwinWrapper(model)
+
+
+def swinv2_base_window16_256_timm(*args, **kwargs):
+    model = timm.create_model(
+        "swinv2_base_window16_256.ms_in1k", features_only=True, pretrained=True
+    )
+    return SwinWrapper(model)
+
+
+def swinv2_large_window16_256_timm(*args, **kwargs):
+    model = timm.create_model(
+        "swinv2_large_window12to16_192to256.ms_in22k_ft_in1k",
+        features_only=True,
+        pretrained=True,
+    )
     return SwinWrapper(model)
