5707ce34129b3f8336a2cab2a0e473411d28f2ba
Tyler Zhu <tyler.zhu@berkeley.edu>
2024-01-23 02:16:08 +0000

    update sweep script and add sweeps for swin xview

--- launch_scripts/cluster/sweep_creator/create_sweeps.py
+++ launch_scripts/cluster/sweep_creator/create_sweeps.py
@@ -25,24 +25,28 @@ def parse_args():
     parser = ArgumentParser("Create sweep files for Gigaformer experiments")
     parser.add_argument(
         "--sweep_options",
+        "-s",
         default=None,
         type=Path,
         help="Path to the file that contains the sweep options",
     )
     parser.add_argument(
         "--base_config",
+        "-b",
         default=None,
         type=Path,
         help="Path to config file upon which to base sweeps",
     )
     parser.add_argument(
         "--output_folder",
+        "-o",
         default=None,
         type=Path,
         help="Path to folder where the sweeps will be saved",
     )
     parser.add_argument(
         "--experiment_base_name",
+        "-e",
         default=None,
         type=str,
         help="Base name for the experiment name",
@@ -62,6 +66,13 @@ opt_key_map = {
     "model.backbone_class": "backbone",
 }
 
+opt_map = {
+    "swinv2_tiny_window16_256_timm_xview": "swinv2-tiny",
+    "swinv2_small_window16_256_timm_xview": "swinv2-small",
+    "swinv2_base_window16_256_timm_xview": "swinv2-base",
+    "swinv2_large_window16_256_timm_xview": "swinv2-large",
+}
+
 
 def main():
     args = parse_args()
@@ -74,8 +85,26 @@ def main():
 
     combs = []
     for options in sweep_options["key_names"]:
-        combs.append(list(product([options["name"]], options["options"])))
+        if options.get("group", None) is not None:
+            all_options = []
+            for group_options in options["group"]:
+                all_options.append(list(group_options.items()))
+            combs.append(all_options)
+        else:
+            combs.append(list(product([options["name"]], options["options"])))
     all_combs = list(product(*combs))
+
+    # Flatten the grouped options 
+    all_combs_temp = []
+    for comb in all_combs:
+        temp = []
+        for opt in comb:
+            if isinstance(opt, list):
+                temp += opt
+            else:
+                temp.append(opt)
+        all_combs_temp.append(temp)
+    all_combs = all_combs_temp
 
     for combination in all_combs:
         modified_config = deepcopy(base_config)
@@ -90,6 +119,8 @@ def main():
 
             key_name = opt_key_map.get(key, key.split(".")[-1])
             key_name = key_name.replace("_", "-")
+
+            opt= opt_map.get(opt, opt)
             if key_name == "blr":
                 experiment_name += f"_{key_name}-{opt:.0e}"
             else:
